26c6d06fa4e693a9fa1f07b6250bad0bd84a41d9
Pacsfury <pacsfury12@gmail.com>
2026-05-11 19:40:12 +0200

    Updated code to conventions and legibility. README for more info

--- rubidung.java
+++ rubidung.java
@@ -1,14 +1,12 @@
 import java.util.Scanner;
 
-public class rubidung {
+public class Rubidung {
 
-    public static void clearScreen() { // Clear the console screen
-
-        System.out.print("\033[H\033[2J");
+    public static void clearScreen() {
+        System.out.print("\033[H\033[2J"); //ANSI escape code to clear the screen and move the cursor to the top-left corner
         System.out.flush();
         
     }
-
 
     public static class Player {
         public int x;
@@ -29,10 +27,12 @@ public class rubidung {
     }
 
     public static class Level {
+
         public int levelNumber;
         public char[][] map;
 
         public Level(int levelNumber) {
+
             this.levelNumber = levelNumber;
  
             switch (levelNumber) {
@@ -76,96 +76,84 @@ public class rubidung {
 
 
     public static boolean playLevel(Level level, Scanner scanner) {
+
         boolean win = false;
-
-        Level level1 = level;
-
-        // World world = new World(level1);
-
         Player player = new Player();
 
-
-        for (int i = 0; i < level1.map.length; i++) {                 // Print the map using nested loops (rows and columns)
-            for (int j = 0; j < level1.map[i].length; j++) {
-                System.out.print(level1.map[i][j] + " ");
+        for (int i = 0; i < level.map.length; i++) { // Print the map using nested loops (rows and columns)
+            for (int j = 0; j < level.map[i].length; j++) {
+                System.out.print(level.map[i][j] + " ");
             }
             System.out.println(); 
         }
 
-        for (int j = 0; j < 10; j++) { // Read and execute user commands
-
-
+        for (int turn = 0; turn < 10; turn++) { // Read and execute user commands
             String command = scanner.nextLine();
 
-            if (j == 0) {        //Clears the screen after first command (which is also cleaned to avoid people writing everything they need)
+            if (turn == 0) {  //Clears the screen after first command (which is also cleaned to avoid people writing everything they need)
                 clearScreen();
             }
 
             if (command.equals("w")) {
+
                 System.out.println("Move up");
                 player.y -= 1;
             } else if (command.equals("a")) {
+
                 System.out.println("Move left");
                 player.x -= 1;
             } else if (command.equals("s")) {
+
                 System.out.println("Move down");
                 player.y += 1;
             } else if (command.equals("d")) {
+
                 System.out.println("Move right");
                 player.x += 1;
             } else {
+
                 System.out.println("Invalid command");
             }
 
-
-            char currentTile = player.getCurrentTile(level1);
+            char currentTile = player.getCurrentTile(level);
 
             if (currentTile == '$') {
                 System.out.println("You win!");
                 win = true;
                 break;
-
-
             } else if (currentTile == '#') {
                 System.out.println("You hit a wall! Game over.");
                 break;
             }
-
         }
 
         if (!win) { //If 10 moves are done, the game ends with a lose message
             System.out.println("Game over. Try again!");
-            
         } 
 
         return win;
-
     }
+
     public static void main(String[] args) {
+        int TOTAL_LEVELS = 3; 
         Scanner sc = new Scanner(System.in);
         boolean win = false;
 
+        Level actualLevel = new Level(1);
+        win = playLevel(actualLevel, sc);
 
-        Level level_act = new Level(1);
-        win =playLevel(level_act, sc);
+        for (int level = 2; level < TOTAL_LEVELS + 1; level++) {
 
-
-
-        for (int k = 2; k < 4; k++) {
             if (win) {
-                level_act = new Level(k);
-                win =playLevel(level_act, sc);
+                actualLevel = new Level(level);
+                win = playLevel(actualLevel, sc);
             } else {
                 break;
             }
         }
 
-        System.out.println("Congratulations! You completed all levels!");
-
-
         clearScreen();
-
+        System.out.println("Congratulations! You completed all levels!");
         sc.close();
-        
     }
 }
